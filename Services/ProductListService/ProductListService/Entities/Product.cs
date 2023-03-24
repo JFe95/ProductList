891b3638328a0c448aae5dad4a001e25d24d30c5
Jake Featherstone <jake.featherstone@tribalgroup.com>
2023-03-24 13:50:36 +0000

    Add Product Entity

---
+++ Services/ProductListService/ProductListService/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductListService.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public bool Priority { get; set; }
+    }
+}
