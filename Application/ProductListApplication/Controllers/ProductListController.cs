cd84742ab99ea61c7731c47c4007d46d0dcf5a70
Jake Featherstone <jake.featherstone@tribalgroup.com>
2023-03-24 16:51:04 +0000

    Add Basic Web Client that can return List of Product Names

---
+++ Application/ProductListApplication/Controllers/ProductListController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductListApplication.Models;
+using ProductListApplication.Services;
+using System.Diagnostics;
+
+namespace ProductListApplication.Controllers
+{
+    public class ProductListController : Controller
+    {
+        private readonly ILogger<ProductListController> _logger;
+
+        public ProductListController(ILogger<ProductListController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new ProductListViewModel{Products = ProductService.GetProductsAsync().GetAwaiter().GetResult()});
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
