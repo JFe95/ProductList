7b09a601c441a2911eb8c8f25ba3f31958e4810b
Jake Featherstone <jake.featherstone@tribalgroup.com>
2023-03-25 11:39:37 +0000

    Move Javascript to site.js

--- Application/ProductListApplication/wwwroot/js/site.js
+++ Application/ProductListApplication/wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function incrementViewCount(i) {
+    var count = document.getElementById('viewCount ' + i);
+    var number = count.innerHTML;
+    number++;
+    count.innerHTML = number;
+}
