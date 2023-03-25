88d99d877b4b70cae470d9d1db38a749ecc85035
Jake Featherstone <jmfe95@googlemail.com>
2023-03-25 16:56:54 +0000

    Home PC Setup (#4)
    * Task3 (#3)
    
    * Move Javascript to site.js
    
    * Add sort options
    
    * Add a highlight to indicate priority products
    
    ---------
    
    Co-authored-by: Jake Featherstone <jake.featherstone@tribalgroup.com>
    
    * gitignore local setting files
    
    * Enable easier setup on new devices
    
    ---------
    
    Co-authored-by: Jake Featherstone <jake.featherstone@tribalgroup.com>

--- Services/ProductListService/ProductListService/Program.cs
+++ Services/ProductListService/ProductListService/Program.cs
@@ -4,30 +4,21 @@ using ProductListService;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ProductContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductList"),
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ProductList"],
         sqlServerOptionsAction: sqlOptions =>
         {
-            sqlOptions.MigrationsAssembly(
-                typeof(Program).GetTypeInfo().Assembly.GetName().Name);
-
-            //Configuring Connection Resiliency:
-            sqlOptions.EnableRetryOnFailure(maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorNumbersToAdd: null);
+            sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+            sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
         });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
